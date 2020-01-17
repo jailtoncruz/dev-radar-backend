49f5cd7c3cdcb590bd15035d689fc9d1b0c40913
Jailton Cruz <jailtoncruz@iMac-de-Jailton.local>
2020-01-17 01:38:34 -0300

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+## BACKEND DA APLICAÇÃO
+
+Este backend foi desenvolvido utilizando NodeJS com express, fazendo conexão ao banco de dados não relacional, MongoDB no MongoDB Atlas.
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+mongoose.connect('mongodb+srv://tom:week12345678@cluster0-lkwxl.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(3333);
